abf445b69f054d65d983c7c15a33b7f55b645bce
shahram <shahramkhaleh2001@gmail.com>
2023-07-31 03:32:31 +0330

    feat:add bg image to home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { Image, SafeAreaView, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text className="text-7xl">Hello World</Text>
+    <View className="flex-1 relative">
+      <StatusBar style="light" />
+      <Image
+        blurRadius={70}
+        className="absolute h-full w-full"
+        source={require('assets/images/bg.png')}
+      />
+      <SafeAreaView className="flex flex-1">
+        {/* search section */}
+        <View style={{ height: '7%' }} className="mx-4 relative z-50">
+          <View
+            className="flex-row justify-end items-center rounded-full"
+            style={{
+              backgroundColor: 'transparent',
+            }}
+          ></View>
+        </View>
+      </SafeAreaView>
     </View>
-  )
+  );
 }
