5459b7b47a560994a7f38f17920905e43349693c
shahram <shahramkhaleh2001@gmail.com>
2023-08-04 21:43:19 +0330

    refactor: update babel config file

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,15 @@ module.exports = function(api) {
             assets:"./assets"
           },
         },
-      ]
+      ],
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }]
     ],
   };
 };
